379a2ab7cb774a3581907100c4358c53e8474980
GuidoFlores <107821723+GuidoFlores@users.noreply.github.com>
2022-11-14 23:16:00 -0500

    Ventana de filtros v1

--- src/main/java/clases/Juego.java
+++ src/main/java/clases/Juego.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.classes;
+package clases;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
---
+++ src/main/java/com/mycompany/prg9/FiltrosController.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.prg9;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.CheckBox;
+import tdas.*;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ * 
+ * @author USER1
+ */
+public class FiltrosController implements Initializable {
+    
+    @FXML
+    private ComboBox cbFecha;
+    
+    @FXML
+    private ComboBox cbCalificacion1;
+    
+    @FXML
+    private ComboBox cbCalificacion2;
+    
+    @FXML
+    private VBox vBGeneros;
+    
+    
+    public ArrayList<String> crearColeccion(){
+        ArrayList<String> fechas = new ArrayList<>();
+        
+        int fechaActual;
+        
+        for(fechaActual=2022; fechaActual>=1950;fechaActual--){
+            fechas.addLast(String.valueOf(fechaActual));
+        }
+        return fechas;
+    }
+    
+    public ArrayList<String> coleccionEstrellas(){
+        
+        ArrayList<String> estrellas= new ArrayList<>();
+        
+        for(int i=5; i>=1; i--){
+            estrellas.addLast(String.valueOf(i));
+        }
+        
+       return estrellas;
+    }
+    
+    public ArrayList<String> coleccionGeneros(){
+        
+        ArrayList<String> generos = new ArrayList<>();
+        
+        generos.add("Aventura");
+        generos.add("Arcade");
+        generos.add("Acción");
+        generos.add("Deportes");
+        generos.add("Estrategia");
+        generos.add("Simulación");
+        
+        return generos;
+                
+    }
+    
+    public void llenarCb(){
+        for(String genero: coleccionGeneros()){
+            CheckBox cb = new CheckBox(genero);
+            vBGeneros.getChildren().add(cb);
+        }
+    }
+    
+   public void llenarCombo(){
+       
+      for(String fecha: crearColeccion()){
+          cbFecha.getItems().add(fecha);
+          
+      }
+   }
+   
+   public void llenarComboEstrellas(){
+       
+       for(String estrella: coleccionEstrellas()){
+           cbCalificacion1.getItems().add(estrella);
+           cbCalificacion2.getItems().add(estrella);
+       }
+   }
+                
+   
+   
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+       llenarCombo();
+       llenarComboEstrellas();
+       llenarCb();
+    }    
+    
+    
+    
+}
